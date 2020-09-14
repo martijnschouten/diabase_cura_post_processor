de36437865b4895a6610b4654a11341adf0f794d
schoutenm <martijn-schouten@hotmail.com>
2020-09-14 08:38:28 +0200

    added new cleaning cycle

--- NIFTyDiabasePostProcessor.py
+++ NIFTyDiabasePostProcessor.py
@@ -6,6 +6,7 @@ import re #To perform the search and replace.
 from ..Script import Script
 import warnings
 
+from UM.Application import Application #To get the current printer's settings.
 from UM.Logger import Logger
 
 class NIFTyDiabasePostProcessor(Script):
@@ -43,6 +44,23 @@ class NIFTyDiabasePostProcessor(Script):
                     "minimum_value": "2",
                     "enabled": "cleaning_T1 == 'Interval'"
                 },
+                "blob_size_T1": {
+                    "label": "Blob size T1",
+                    "description": "How much material in mm should be used to make the blob",
+                    "type": "float",
+                    "default_value": 15.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T1 != 'Off' and cleaning_style == 'New'"
+                },
+                "blob_speed_T1": {
+                    "label": "Blob extrusion speed T1",
+                    "description": "How fast the material should be extruded while making a blob",
+                    "unit": "mm/s",
+                    "type": "float",
+                    "default_value": 30.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T1 != 'Off' and cleaning_style == 'New'"
+                },
                 "cleaning_T2": {
                     "label": "Cleaning of T2",
                     "description": "Cleaning behaviour for Tool 2",
@@ -62,6 +80,23 @@ class NIFTyDiabasePostProcessor(Script):
                     "default_value": 3,
                     "minimum_value": "2",
                     "enabled": "cleaning_T2 == 'Interval'"
+                },
+                "blob_size_T2": {
+                    "label": "Blob size T2",
+                    "description": "How much material in mm should be used to make the blob",
+                    "type": "float",
+                    "default_value": 15.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T2 != 'Off' and cleaning_style == 'New'"
+                },
+                "blob_speed_T2": {
+                    "label": "Blob extrusion speed T2",
+                    "description": "How fast the material should be extruded while making a blob",
+                    "unit": "mm/s",
+                    "type": "float",
+                    "default_value": 30.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T2 != 'Off' and cleaning_style == 'New'"
                 },
                 "cleaning_T3": {
                     "label": "Cleaning of T3",
@@ -83,6 +118,23 @@ class NIFTyDiabasePostProcessor(Script):
                     "minimum_value": "2",
                     "enabled": "cleaning_T3 == 'Interval'"
                 },
+                "blob_size_T3": {
+                    "label": "Blob size T3",
+                    "description": "How much material in mm should be used to make the blob",
+                    "type": "float",
+                    "default_value": 15.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T3 != 'Off' and cleaning_style == 'New'"
+                },
+                "blob_speed_T3": {
+                    "label": "Blob extrusion speed T3",
+                    "description": "How fast the material should be extruded while making a blob",
+                    "unit": "mm/s",
+                    "type": "float",
+                    "default_value": 30.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T3 != 'Off' and cleaning_style == 'New'"
+                },
                 "cleaning_T4": {
                     "label": "Cleaning of T4",
                     "description": "Cleaning behaviour for Tool 4",
@@ -103,6 +155,23 @@ class NIFTyDiabasePostProcessor(Script):
                     "minimum_value": "2",
                     "enabled": "cleaning_T4 == 'Interval'"
                 },
+                "blob_size_T4": {
+                    "label": "Blob size T4",
+                    "description": "How much material in mm should be used to make the blob",
+                    "type": "float",
+                    "default_value": 15.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T4 != 'Off' and cleaning_style == 'New'"
+                },
+                "blob_speed_T4": {
+                    "label": "Blob extrusion speed T4",
+                    "description": "How fast the material should be extruded while making a blob",
+                    "unit": "mm/s",
+                    "type": "float",
+                    "default_value": 30.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T4 != 'Off' and cleaning_style == 'New'"
+                },
                 "cleaning_T5": {
                     "label": "Cleaning of T5",
                     "description": "Cleaning behaviour for Tool 5",
@@ -122,6 +191,33 @@ class NIFTyDiabasePostProcessor(Script):
                     "default_value": 3,
                     "minimum_value": "2",
                     "enabled": "cleaning_T5 == 'Interval'"
+                },
+                "blob_size_T5": {
+                    "label": "Blob size T5",
+                    "description": "How much material in mm should be used to make the blob",
+                    "type": "float",
+                    "default_value": 15.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T5 != 'Off' and cleaning_style == 'New'"
+                },
+                "blob_speed_T5": {
+                    "label": "Blob extrusion speed T5",
+                    "description": "How fast the material should be extruded while making a blob",
+                    "unit": "mm/s",
+                    "type": "float",
+                    "default_value": 30.0,
+                    "minimum_value": "0",
+                    "enabled": "cleaning_T5 != 'Off' and cleaning_style == 'New'"
+                },
+                "cleaning_style": {
+                    "label": "Cleaning style",
+                    "description": "Cleaning style",
+                    "type": "enum",
+                    "options": {
+                        "Old": "Old",
+                        "New": "New"
+                    },
+                    "default_value": "New"
                 }
             }
         }"""
@@ -132,32 +228,49 @@ class NIFTyDiabasePostProcessor(Script):
             #start by increasing number 4 and then go down. If one would start at 0 the result would end up as 1
             for i1 in range(4,-1,-1):
                 data[layer_number] = re.sub('T'+str(i1),'T'+str(i1+1), data[layer_number]) #increase all tool numbers by one
-            
+        
+        cleaning_style = self.getSettingValueByKey("cleaning_style")
+        initial_extruder = self.find_initial_extruder(data)
+        Logger.log("i", 'initial extruder: ' + str(initial_extruder))
+        
         cleaning_T1 = self.getSettingValueByKey("cleaning_T1")
         interval_T1 = self.getSettingValueByKey("interval_T1")
-        data = self.insert_cleaning(data,cleaning_T1,interval_T1,1)
+        blob_size_T1 = self.getSettingValueByKey("blob_size_T1")
+        blob_speed_T1 = self.getSettingValueByKey("blob_speed_T1")
+        data = self.insert_cleaning(data,cleaning_T1,interval_T1,blob_size_T1,blob_speed_T1,cleaning_style,initial_extruder,1)
         
         cleaning_T2 = self.getSettingValueByKey("cleaning_T2")
         interval_T2 = self.getSettingValueByKey("interval_T2")
-        data = self.insert_cleaning(data,cleaning_T2,interval_T2,2)
+        blob_size_T2 = self.getSettingValueByKey("blob_size_T2")
+        blob_speed_T2 = self.getSettingValueByKey("blob_speed_T2")
+        data = self.insert_cleaning(data,cleaning_T2,interval_T2,blob_size_T2,blob_speed_T2,cleaning_style,initial_extruder,2)
         
         cleaning_T3 = self.getSettingValueByKey("cleaning_T3")
         interval_T3 = self.getSettingValueByKey("interval_T3")
-        data = self.insert_cleaning(data,cleaning_T3,interval_T3,3)
+        blob_size_T3 = self.getSettingValueByKey("blob_size_T3")
+        blob_speed_T3 = self.getSettingValueByKey("blob_speed_T3")
+        data = self.insert_cleaning(data,cleaning_T3,interval_T3,blob_size_T3,blob_speed_T3,cleaning_style,initial_extruder,3)
         
         cleaning_T4 = self.getSettingValueByKey("cleaning_T4")
         interval_T4 = self.getSettingValueByKey("interval_T4")
-        data = self.insert_cleaning(data,cleaning_T4,interval_T4,4)
+        blob_size_T4 = self.getSettingValueByKey("blob_size_T4")
+        blob_speed_T4 = self.getSettingValueByKey("blob_speed_T4")
+        data = self.insert_cleaning(data,cleaning_T4,interval_T4,blob_size_T4,blob_speed_T4,cleaning_style,initial_extruder,4)
         
         cleaning_T5 = self.getSettingValueByKey("cleaning_T5")
         interval_T5 = self.getSettingValueByKey("interval_T5")
-        data = self.insert_cleaning(data,cleaning_T5,interval_T5,5)
+        blob_size_T5 = self.getSettingValueByKey("blob_size_T5")
+        blob_speed_T5 = self.getSettingValueByKey("blob_speed_T5")
+        data = self.insert_cleaning(data,cleaning_T5,interval_T5,blob_size_T5,blob_speed_T5,cleaning_style,initial_extruder,5)
+        
+        
+        
         
         data[0] = '; postproccesed using the NIFTy Diabase Post processor\n' + data[0]
 
         return data
         
-    def insert_cleaning(self,data,behaviour,interval,tool_number):
+    def insert_cleaning(self,data,behaviour,interval,blob_size,blob_speed,cleaning_style,initial_extruder,tool_number):
         
         if behaviour == 'Off':
             data[0] = '; Tool ' + str(tool_number) + ' will not be cleaned \n' + data[0]
@@ -170,8 +283,9 @@ class NIFTyDiabasePostProcessor(Script):
             data[0] = '; Tool ' + str(tool_number) + ' will always be cleaned \n' + data[0]
         else:
             raise Exception('unknown cleaning behaviour selected')
-            
-            
+               
+        
+        
         tool_change = 0
         passed_initialisation_toolchange = False
         for layer_number, layer in enumerate(data):
@@ -190,8 +304,8 @@ class NIFTyDiabasePostProcessor(Script):
                 for line_number, layer_line in enumerate(layer_lines):
                     if state == 'found_nothing':
                         if layer_line == 'T'+str(tool_number):
-                            #ignore the first toolchange
-                            if passed_initialisation_toolchange == True:
+                            #ignore the very first toolchange
+                            if passed_initialisation_toolchange == True or initial_extruder != tool_number:
                                 #found the tool change
                                 state = 'found_the_tool_change'
                                 tool_change = tool_change + 1
@@ -225,7 +339,30 @@ class NIFTyDiabasePostProcessor(Script):
                         
                 #insert the cleaning line
                 if cleaning_line > -1:
-                    layer_lines.insert(cleaning_line,'M98 P"tprime' + str(tool_number) + '.g" ; cleaning ')
+                    if cleaning_style == 'Old':
+                        layer_lines.insert(cleaning_line,'M98 P"tprime' + str(tool_number) + '.g" ; cleaning ')
+                    else:
+                        toolchange_retraction_distance = Application.getInstance().getGlobalContainerStack().getProperty("switch_extruder_retraction_amount", "value")
+                        toolchange_retraction_speed = Application.getInstance().getGlobalContainerStack().getProperty("switch_extruder_retraction_speed", "value")
+                        toolchange_extrusion_speed = Application.getInstance().getGlobalContainerStack().getProperty("switch_extruder_prime_speed", "value")
+                        toolchange_extra_extrusion = Application.getInstance().getGlobalContainerStack().getProperty("switch_extruder_extra_prime_amount", "value")
+                        
+                        Logger.log("i", 'layer_number: ' + str(layer_number))
+                        Logger.log("i", 'initial extruder: ' + str(initial_extruder))
+                        Logger.log("i", 'tool number: ' + str(tool_number))
+                        toolchange_string = 'M98 P"tprime' + str(tool_number) + 'pre.g" ; move to cleaning station\n' + \
+                                                'G1 E' + str(toolchange_retraction_distance+toolchange_extra_extrusion) + ' F' + str(toolchange_extrusion_speed*60) + '\n' + \
+                                                'M42 P20 S0.75\n' + \
+                                                'G1 E' + str(blob_size) + ' F' + str(blob_speed*60) + '\n' + \
+                                                'M400\n' + \
+                                                'G4 P3000 ;\n' + \
+                                                'M42 P20 S0\n' + \
+                                                'G1 E-' + str(toolchange_retraction_distance) + ' F' + str(toolchange_retraction_speed*60) + '\n' + \
+                                                'M98 P"tprime' + str(tool_number) + 'post.g" ; move back'
+                        if layer_number == 1 and initial_extruder == tool_number:
+                            toolchange_string = toolchange_string + \
+                                                '\nG1 E' + str(toolchange_retraction_distance+toolchange_extra_extrusion) + ' F' + str(toolchange_extrusion_speed*60)
+                        layer_lines.insert(cleaning_line,toolchange_string)
                 #delete the original toolchange.
                 if tool_change_line > -1:                        
                     layer_lines.pop(tool_change_line)
@@ -234,4 +371,9 @@ class NIFTyDiabasePostProcessor(Script):
                 data[layer_number] = '\n'.join(layer_lines)
         return data
                     
-        
+    def find_initial_extruder(self,data):
+        for layer_number, layer in enumerate(data):
+            layer_lines = layer.splitlines()
+            for line_number, layer_line in enumerate(layer_lines):
+                if layer_line[0] == 'T':
+                    return int(layer_line[1])
