7bf0b5b360b14481346c9af9b0c486b45a6da67b
schoutenm <martijn-schouten@hotmail.com>
2022-07-26 14:27:29 +0200

    removed tool selection at the beginning

--- NIFTyDiabasePostProcessor.py
+++ NIFTyDiabasePostProcessor.py
@@ -24,13 +24,28 @@ class NIFTyDiabasePostProcessor(Script):
             "version": 2,
             "settings":
             {
+                "Z_offset_T1": {
+                    "label": "Z offset T1",
+                    "description": "Additional Z offset T1",
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 0.0
+                },
             }
         }"""
 
     def execute(self, data):
-        #increase each tool number by one
-        data[0] = 'G28\n; home the printer before cura does the T1' + data[0]
+        #replace the tool selection command all the way at the beginning with a homing command
+        layer0_lines = data[1].splitlines()
+        #Logger.log("e", 'lines in first layer: ' + str(len(layer0_lines)))
+        for i1 in range(len(layer0_lines)):
+            #Logger.log("e", 'First letter: ' + layer0_lines[i1][0])
+            if layer0_lines[i1][0] == 'T':
+                layer0_lines[i1] = 'G28'
+                break
+        data[1] = '\n'.join(layer0_lines)
         
+        #increase each tool number by one
         for layer_number, layer in enumerate(data):
             #start by increasing number 5 and then go down. If one would start at 0 the result would end up as 1
             for i1 in range(5,-1,-1):
